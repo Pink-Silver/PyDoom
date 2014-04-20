23a52d64d0ad653b4084663734915c5f99fdadc8
Kate Stone <exe3057@gmail.com>
2014-04-19 23:58:50 -0400

    Build script for making a Python source container

---
+++ MakeProgramZip.py
@@ -0,0 +1,20 @@
+import zipfile, glob, os.path
+from sys import argv
+
+def main (outfile):
+    print ("Writing {}...".format (outfile))
+
+    with zipfile.ZipFile (outfile, "w") as openedzip:
+        fnames = glob.iglob ("./pysrc/*")
+        total = 0
+
+        for fn in fnames:
+            name = "".join (os.path.split (fn)[1:])
+            print ("- Adding {}".format (name))
+            openedzip.write (fn, arcname=name)
+            total += 1
+
+    print ("{} total file(s) written.".format (total))
+
+if __name__ == "__main__":
+    main (argv[1])
