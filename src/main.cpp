53a7009988107ab04bd069bf6a0e7386dee124f0
Kate Stone <exe3057@gmail.com>
2014-04-20 19:19:18 -0400

    Some basic code to run a script from the exe

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,14 @@
 
 int main (int argc, char *argv[])
 {
+    Py_SetProgramName (L"PyDoom");
+    Py_Initialize ();
+    
+    PyRun_SimpleString ("\
+import sys\n\
+sys.path = [\"PyDoom.zip\"] + sys.path\n\
+del sys\n\
+import pydoom_main\n\
+pydoom_main.main ()\n");
     return 0;
 }
