a96a093e06847c4956259d17e26071bc664bd6bd
Kate Stone <exe3057@gmail.com>
2014-04-27 14:31:38 -0400

    Initialize path and arguments

--- pysrc/pydoom_main.py
+++ pysrc/pydoom_main.py
@@ -1,6 +1,9 @@
 from pydoom_version import GITVERSION
 from time import sleep
+from sys import argv, path
 
 def main ():
     print ("=== PyDoom revision {} ===".format (GITVERSION))
+    print ("Received arguments: {} ({} total)".format (" ".join (argv), len (argv)))
+    print ("Current Python paths: {} ({} total)".format (", ".join (path), len (path)))
     sleep (20)
--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,22 @@ int main (int argc, char *argv[])
     Py_SetProgramName (L"PyDoom");
     Py_Initialize ();
     
-    PyRun_SimpleString ("\
-import sys\n\
-sys.path = [\"PyDoom.zip\"] + sys.path\n\
-del sys\n\
-import pydoom_main\n\
-pydoom_main.main ()\n");
+    // Coerce arguments
+    PyObject *args = PyList_New ((Py_ssize_t)argc);
+    for (Py_ssize_t i = 0; i < argc; ++i)
+    {
+        PyObject *arg = Py_BuildValue ("s", argv[i]);
+        PyList_SetItem (args, i, arg);
+    }
+    
+    PySys_SetObject ("argv", args);
+    
+    // Path alteration
+    PySys_SetPath (L"PyDoom.zip");
+    
+    // Jump into the main program
+    PyRun_SimpleString ("import pydoom_main\npydoom_main.main ()\n");
+    
+    Py_Exit (0);
     return 0;
 }
