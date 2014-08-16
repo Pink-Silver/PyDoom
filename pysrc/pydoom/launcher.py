f2c6dcc64ffd0cd0dc06bd8dba2dde029d5a126e
Kate Stone <exe3057@gmail.com>
2014-08-15 20:56:06 -0400

    Give launcher.py license info, update TODO

--- pysrc/pydoom/launcher.py
+++ pysrc/pydoom/launcher.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2014, Kate Stone
+# All rights reserved.
+#
+# This file is covered by the 3-clause BSD license.
+# See the LICENSE file in this program's distribution for details.
+
 import tkinter
 import tkinter.filedialog
 from sys import argv
