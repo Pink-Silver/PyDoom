734e7a2dc4f0939331e79bb2529528193deb8a26
Kate Fox <exe3057@gmail.com>
2016-10-01 21:06:41 -0400

    A stub for core code

---
+++ pydoom/core.pyx
@@ -0,0 +1,11 @@
+#!python3
+#cython: language_level=3
+
+# Copyright (c) 2016, Kate Fox
+# All rights reserved.
+#
+# This file is covered by the 3-clause BSD license.
+# See the LICENSE file in this program's distribution for details.
+
+class PyDoomCore:
+    pass
